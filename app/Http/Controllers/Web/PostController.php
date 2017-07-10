00843e248180e38853fe4df29761725a400639fb
Pham Van Truong <nhandangkytuvan@gmail.com>
2017-07-10 15:09:04 +0700

    ok

--- app/Http/Controllers/Web/PostController.php
+++ app/Http/Controllers/Web/PostController.php
@@ -3,28 +3,41 @@ namespace App\Http\Controllers\Web;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Post;
-use App\Setting;
-use App\Events\VisitSumEvent;
-use App\Events\VisitPostEvent;
 use BrowserDetect;
 class PostController extends Controller{
+    public $term = null;
+    public $term_parent = null;
+    public $post_pre = null;
+    public $post_next = null;
+    public $post = null;
+    public $post_lienquans = null;
+
     public function show($post_link,Request $request){
         $array_link = explode('-',$post_link);
         $post_id = end($array_link);
         //----------------------------------
-    	$setting = Setting::first();
-        //event(new VisitSumEvent($setting));
     	$post = Post::find($post_id);
         if(!$post){
             return redirect('/');
         }
         // --------------
-        //event(new VisitPostEvent($post));
-        // ------------
-        $data['post'] = $post;
+        $term = $post->term;
+        $term_parent = $term ? $term->parent : null;
+        $post_pre = $term->post()->where('id','<',$post->id)->orderBy('id','desc')->first();
+        $post_next = $term->post()->where('id','>',$post->id)->orderBy('id','asc')->first();
+        //-----------------------------
         if(BrowserDetect::isDesktop()){
+            $data['post']           = $post;
+            $data['term']           = $term;
+            $data['term_parent']    = $term_parent;
+            $data['post_pre']       = $post_pre;
+            $data['post_next']      = $post_next;
         	return view('web.desktop.post3',['data'=>$data]); 
         }else{
+            $post_lienquans = $term ? $term->post()->where('id','<>',$post->id)->limit(6)->get() : null;
+            $data['post']           = $post;
+            $data['term']           = $term;
+            $data['post_lienquans'] = $post_lienquans;
         	return view('web.mobile.post',['data'=>$data]); 
         }
     }
--- app/Http/Controllers/Web/TermController.php
+++ app/Http/Controllers/Web/TermController.php
@@ -4,9 +4,14 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Term;
 use App;
-use App\Events\VisitTermEvent;
 use BrowserDetect;
 class TermController extends Controller{
+    public $term = null;
+    public $term_parent = null;
+    public $term_childs = null;
+    public $post_recent_terms = null;
+    public $post_recent_childs = null;
+
     public function show($term_link,Request $request){
         $array_link = explode('-',$term_link);
         $term_id = end($array_link);
@@ -15,64 +20,70 @@ class TermController extends Controller{
             return redirect('/');
         }
         // -----------
-        //event(new VisitTermEvent($term));
-        // -----------
-        $data['term'] = $term;
         if(BrowserDetect::isDesktop()){
             switch ($term_id) {
                 case 33:
+                    $data['term'] = $term;
                     return view('web.desktop.thietbi',['data'=>$data]); 
-                    break;
-                case 32:
-                    return view('web.desktop.term3',['data'=>$data]); 
-                    break;
-                case 34:
-                    return view('web.desktop.term3',['data'=>$data]); 
                     break;
                 // termParent
                 case 22:
-                    $termChilds = $term->children;
-                    $postNews = App\Post::whereIn('term_id',[23,24,25,26])->limit(3)->orderBy('id','desc')->get();
-                    return view('web.desktop.termParent',['data'=>$data,'postNews'=>$postNews,'termChilds'=>$termChilds]);
+                    $term_childs = $term->children;
+                    $post_recent_terms = App\Post::whereIn('term_id',[23,24,25,26])->limit(3)->orderBy('id','desc')->get();
+                    $data['term'] = $term;
+                    $data['term_childs'] = $term_childs;
+                    $data['post_recent_terms'] = $post_recent_terms;
+                    return view('web.desktop.termParent',['data'=>$data]);
                     break;
                 case 17:
-                    $termChilds = $term->children;
-                    $postNews = App\Post::whereIn('term_id',[18,19,20,21])->limit(3)->orderBy('id','desc')->get();
-                    return view('web.desktop.termParent',['data'=>$data,'postNews'=>$postNews,'termChilds'=>$termChilds]);
+                    $term_childs = $term->children;
+                    $post_recent_terms = App\Post::whereIn('term_id',[18,19,20,21])->limit(3)->orderBy('id','desc')->get();
+                    $data['term'] = $term;
+                    $data['term_childs'] = $term_childs;
+                    $data['post_recent_terms'] = $post_recent_terms;
+                    return view('web.desktop.termParent',['data'=>$data]);
                     break;
                 case 12:
-                    $termChilds = $term->children;
-                    $postNews = App\Post::whereIn('term_id',[13,14,15,16])->limit(3)->orderBy('id','desc')->get();
-                    return view('web.desktop.termParent',['data'=>$data,'postNews'=>$postNews,'termChilds'=>$termChilds]);
+                    $term_childs = $term->children;
+                    $post_recent_terms = App\Post::whereIn('term_id',[13,14,15,16])->limit(3)->orderBy('id','desc')->get();
+                    $data['term'] = $term;
+                    $data['term_childs'] = $term_childs;
+                    $data['post_recent_terms'] = $post_recent_terms;
+                    return view('web.desktop.termParent',['data'=>$data]);
                     break;
                 case 7:
-                    $termChilds = $term->children;
-                    $postNews = App\Post::whereIn('term_id',[8,9,10,35])->limit(3)->orderBy('id','desc')->get();
-                    return view('web.desktop.termParent',['data'=>$data,'postNews'=>$postNews,'termChilds'=>$termChilds]);
+                    $term_childs = $term->children;
+                    $post_recent_terms = App\Post::whereIn('term_id',[8,9,10,35])->limit(3)->orderBy('id','desc')->get();
+                    $data['term'] = $term;
+                    $data['term_childs'] = $term_childs;
+                    $data['post_recent_terms'] = $post_recent_terms;
+                    return view('web.desktop.termParent',['data'=>$data]);
                     break;
                 case 2:
-                    $termChilds = $term->children;
-                    $postNews = App\Post::whereIn('term_id',[3,4,5,6])->limit(3)->orderBy('id','desc')->get();
-                    return view('web.desktop.termParent',['data'=>$data,'postNews'=>$postNews,'termChilds'=>$termChilds]);
+                    $term_childs = $term->children;
+                    $post_recent_terms = App\Post::whereIn('term_id',[3,4,5,6])->limit(3)->orderBy('id','desc')->get();
+                    $data['term'] = $term;
+                    $data['term_childs'] = $term_childs;
+                    $data['post_recent_terms'] = $post_recent_terms;
+                    return view('web.desktop.termParent',['data'=>$data]);
                     break;
                 // endtermParent
                 default:
+                    $term_parent = $term->parent;
+                    $post_terms = $term->post()->paginate(15);
+                    $data['term'] = $term;
+                    $data['term_parent'] = $term_parent;
+                    $data['post_terms'] = $post_terms;
                     return view('web.desktop.term3',['data'=>$data]); 
                     break;
             }
         }else{
             switch ($term_id) {
-                case 33:
-                    return view('web.mobile.term',['data'=>$data]);
-                    break;
-                case 32:
-                    return view('web.mobile.kythuat',['data'=>$data]);
-                    break;
-                case 34:
-                    return view('web.mobile.hoiphuc',['data'=>$data]); 
-                    break;
                 default:
-                    return view('web.mobile.term',['data'=>$data]);  
+                    $post_terms = $term->post()->paginate(6);
+                    $data['term'] = $term;
+                    $data['post_terms'] = $post_terms;
+                    return view('web.mobile.term',['data'=>$data]);
                     break;
             }
         }
